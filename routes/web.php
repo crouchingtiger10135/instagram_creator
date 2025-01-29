1398424b0eeb3c513da5cc671cbfe88527e551b3
crouchingtiger10135 <123414776+crouchingtiger10135@users.noreply.github.com>
2025-01-30 08:46:37 +1100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ImageController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 // Require login + email verification for these
