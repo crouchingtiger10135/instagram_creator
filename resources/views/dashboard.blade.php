c1d5701e9cdc677d766358e8524cf423a5406749
crouchingtiger10135 <123414776+crouchingtiger10135@users.noreply.github.com>
2025-01-31 08:22:36 +1100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
     {{-- PAGE HEADER --}}
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Your Dashboard') }}
+            {{ __('Your Feed') }}
         </h2>
     </x-slot>
 
