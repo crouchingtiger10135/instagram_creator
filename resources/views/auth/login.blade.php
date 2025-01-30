ddb680a32420212ccc1fcc621fd8921b6e5b9a6d
crouchingtiger10135 <123414776+crouchingtiger10135@users.noreply.github.com>
2025-01-30 17:46:50 +1100

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,13 +23,6 @@
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
-
-         <div class="flex items-center justify-end mt-4">
-        @if (Route::has('register'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
-                    {{ __('Need an account?') }}
-                </a>
-            @endif
              
         <!-- Remember Me -->
         <div class="block mt-4">
@@ -38,6 +31,13 @@
                 <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
         </div>
+
+          <div class="block mt-4">
+        @if (Route::has('register'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('Need an account?') }}
+                </a>
+            @endif
         
 
         <div class="flex items-center justify-end mt-4">
